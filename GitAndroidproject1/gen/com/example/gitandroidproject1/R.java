e23dc390170c7e6ee8a08e68102e76870dec2940
siddik3436 <siddik3436@gmail.com>
2015-03-20 22:29:23 +0600

    add third activity

--- GitAndroidproject1/gen/com/example/gitandroidproject1/R.java
+++ GitAndroidproject1/gen/com/example/gitandroidproject1/R.java
@@ -1743,7 +1743,8 @@ containing a value of this type.
         public static final int abc_simple_dropdown_hint=0x7f030016;
         public static final int activity_main=0x7f030017;
         public static final int activity_second=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_third=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1806,6 +1807,7 @@ containing a value of this type.
         public static final int button_text_android_project1_mainactivity=0x7f0a0013;
         public static final int hello_world=0x7f0a0011;
         public static final int title_activity_second=0x7f0a0012;
+        public static final int title_activity_third=0x7f0a0014;
     }
     public static final class style {
         /** 
